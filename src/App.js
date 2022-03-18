82fe40d270c1100d0cb3597567e0ce915d540926
Paulo <pmarsiglia93@gmail.com>
2022-03-18 04:16:11 -0300

    pmarsiglia93 [TASK: ADD O PROGRAMA GORJETA]

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import styled from 'styled-components';
+
+
+const Input = styled.input`
+  width:400px;
+  height:30px;
+  font-size:16px;
+  padding:10px;
+  border:1px solid #000;
+`;
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+  const [pct, setPct] = useState(10);
+  const [conta, setConta] = useState(0);
+
+
+  return ( 
+    <>    
+          <p>Quanto deu a conta?</p>
+      <Input type="number" value={conta} onChange={(e)=>setConta(parseFloat(e.target.value))} />
+      
+      <p>Qual a porcentagem de gorjeta?</p>
+      <Input type="number" value={pct} onChange={(e)=>setPct(parseFloat(e.target.value))} />
+      
+      <hr/>
+
+      {conta > 0 &&
+        <>
+          <p>Sub-total: R$ {conta}</p>
+          <p>Gorjeta ({pct}%): R$ {(pct/100) * conta}</p>
+          <h3>Total: R$ {conta + ((pct/100) * conta)}</h3>
+        </>
+      }
+          
+    </>
+  );  
 }
 
 export default App;
